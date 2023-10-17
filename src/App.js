2142d1a1702897ee5f451a1c694ee28b79a30811
Martin Aja <martinjoseaja@gmail.com>
2023-10-17 17:06:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
-import Inicio from './Inicio/Inicio';
-import Empresas from './Inicio/Empresas';
-import Plantas from './Inicio/Plantas';
-import Matrices from './Inicio/Matrices';
-import Normas from './Inicio/Normas';
+import Inicio from './Pages/Inicio';
+import Resumen from './Pages/Resumen';
+import Wizard from './Pages/Wizard'
+
 
 function App() {
   return (
     <Router>
       <Routes >
-        <Route exact path="/" element={<Inicio/>} />
-        <Route path="/empresas" element={<Empresas/>} />
-        <Route path="/plantas" element={<Plantas/>} />
-        <Route path="/matrices" element={<Matrices/>} />
-        <Route path="/normas" element={<Normas/>} />
-      </Routes >
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/resumen" element={<Resumen/>} />
+        <Route path="/wizard" element={<Wizard/>} />
+      </Routes>
     </Router>
   );
 }
